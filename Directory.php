d49715b6478ed8893237cbfc7313e9791b52b728
stevleibelt <nvb@localhost.localdomain>
2013-05-17 17:41:39 +0200

    Created empty directory class

---
+++ Directory.php
@@ -0,0 +1,156 @@
+<?php
+
+namespace Net\Bazzline\Component\Filesystem;
+
+use RuntimeException;
+use InvalidArgumentException;
+
+/**
+ * Class Directory
+ *
+ * @package Net\Bazzline\Component\Filesystem
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-05-17
+ */
+class Directory
+{
+    public function isReadable()
+    {
+
+    }
+
+    public function isWriteable()
+    {
+
+    }
+
+    public function isExecutable()
+    {
+
+    }
+
+    public function hasContent()
+    {
+
+    }
+
+    public function hasFiles()
+    {
+
+    }
+
+    public function hasDirectories()
+    {
+
+    }
+
+    public function getContent()
+    {
+
+    }
+
+    public function getFiles()
+    {
+
+    }
+
+    public function getDirectories()
+    {
+
+    }
+
+    public function addFile(File $file)
+    {
+
+    }
+
+    public function addDirectory(Directory $directory)
+    {
+
+    }
+
+    public function removeFile(File $file)
+    {
+
+    }
+
+    public function removeDirectory(Directory $directory)
+    {
+
+    }
+
+    public function getModificationTime()
+    {
+
+    }
+
+    public function getModificationDate($format = 'Y-m-d H:i:s')
+    {
+
+    }
+
+    public function getLastAccessTime()
+    {
+
+    }
+
+    public function getLastAccessDate($format = 'Y-m-d H:i:s')
+    {
+
+    }
+
+    public function getCreateTime()
+    {
+
+    }
+
+    public function getCreateDate($format = 'Y-m-d H:i:s')
+    {
+
+    }
+
+    public function setName($name)
+    {
+
+    }
+
+    public function getName()
+    {
+
+    }
+
+    public function setPath($path)
+    {
+
+    }
+
+    public function getPath()
+    {
+
+    }
+
+    public function touch()
+    {
+
+    }
+
+    public function write()
+    {
+
+    }
+
+    public function overwrite()
+    {
+
+    }
+
+    public function read()
+    {
+
+    }
+
+    public function isNew()
+    {
+
+    }
+}
---
+++ FilesystemInterface.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: nvb
+ * Date: 5/17/13
+ * Time: 5:41 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+namespace Net\Bazzline\Component\Filesystem;
+
+
+interface FilesystemInterface {
+
+}
