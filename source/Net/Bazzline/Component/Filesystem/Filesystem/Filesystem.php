adbfbcd45621633e94a6dcd269cf016455f73982
stevleibelt <stev@leibelt.de>
2013-06-15 23:34:22 +0200

    Moved method declaration to interface

--- source/Net/Bazzline/Component/Filesystem/Filesystem/Filesystem.php
+++ source/Net/Bazzline/Component/Filesystem/Filesystem/Filesystem.php
@@ -6,7 +6,6 @@
 namespace Net\Bazzline\Component\Filesystem\Filesystem;
 
 use InvalidArgumentException;
-use Symfony\Component\Filesystem\Exception\IOException;
 use Symfony\Component\Filesystem\Filesystem as SymfonyFilesystem;
 
 /**
@@ -18,15 +17,7 @@ use Symfony\Component\Filesystem\Filesystem as SymfonyFilesystem;
 class Filesystem extends SymfonyFilesystem
 {
     /**
-     * Returns relative $path path to current working directory
-     *
-     * @param string $path - the path that has to be converted into a relative
-     *  path from the current working directory
-     *
-     * @return string
-     * @throws \InvalidArgumentException;
-     * @author stev leibelt
-     * @since 2013-04-25
+     * {$inheritDoc}
      */
     public function makeRelativePathToCurrentWorkingDirectory($path)
     {
@@ -34,15 +25,7 @@ class Filesystem extends SymfonyFilesystem
     }
 
     /**
-     * Returns relative current working directory path to path
-     *
-     * @param string $path - the path where the current working directory has
-     *  to be made relative
-     *
-     * @return string
-     * @throws \InvalidArgumentException;
-     * @author stev leibelt
-     * @since 2013-04-25
+     * {$inheritDoc}
      */
     public function makeCurrentWorkingDirectoryRelativeToPath($path)
     {
@@ -50,15 +33,7 @@ class Filesystem extends SymfonyFilesystem
     }
 
     /**
-     * Converts given $endPath to a relative path to $startPath
-     *
-     * @param string $startPath - the path where $endPath should be relative to
-     * @param string $endPath - the path that should be relative to $startPath
-     *
-     * @return string
-     * @throws \InvalidArgumentException;
-     * @author stev leibelt
-     * @since 2013-04-25
+     * {$inheritDoc}
      */
     public function makePathRelative($startPath, $endPath)
     {
--- source/Net/Bazzline/Component/Filesystem/Filesystem/FilesystemInterface.php
+++ source/Net/Bazzline/Component/Filesystem/Filesystem/FilesystemInterface.php
@@ -13,4 +13,44 @@ namespace Net\Bazzline\Component\Filesystem\Filesystem;
  * @author stev leibelt <artodeto@arcor.de>
  * @since 2013-06-15
  */
-interface FilesystemInterface {}
+interface FilesystemInterface
+{
+    /**
+     * Returns relative $path path to current working directory
+     *
+     * @param string $path - the path that has to be converted into a relative
+     *  path from the current working directory
+     *
+     * @return string
+     * @throws \InvalidArgumentException;
+     * @author stev leibelt
+     * @since 2013-04-25
+     */
+    public function makeRelativePathToCurrentWorkingDirectory($path);
+
+    /**
+     * Returns relative current working directory path to path
+     *
+     * @param string $path - the path where the current working directory has
+     *  to be made relative
+     *
+     * @return string
+     * @throws \InvalidArgumentException;
+     * @author stev leibelt
+     * @since 2013-04-25
+     */
+    public function makeCurrentWorkingDirectoryRelativeToPath($path);
+
+    /**
+     * Converts given $endPath to a relative path to $startPath
+     *
+     * @param string $startPath - the path where $endPath should be relative to
+     * @param string $endPath - the path that should be relative to $startPath
+     *
+     * @return string
+     * @throws \InvalidArgumentException;
+     * @author stev leibelt
+     * @since 2013-04-25
+     */
+    public function makePathRelative($startPath, $endPath);
+}
