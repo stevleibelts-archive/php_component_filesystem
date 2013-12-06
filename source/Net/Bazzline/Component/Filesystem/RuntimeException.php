758242b37f4a9eb85aba361ff75851be40ec47a0
stevleibelt <stev@leibelt.de>
2013-12-06 18:17:34 +0100

    added runtime exception

---
+++ source/Net/Bazzline/Component/Filesystem/RuntimeException.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-12-06
+ */
+
+namespace Net\Bazzline\Component\Filesystem;
+
+use RuntimeException as ParentClass;
+
+/**
+ * Class RuntimeException
+ *
+ * @package Net\Bazzline\Component\Filesystem
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-12-06
+ */
+class RuntimeException extends ParentClass
+{
+} 
