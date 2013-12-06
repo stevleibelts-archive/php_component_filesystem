84c2029e80ccbc0ec51b9f14e181b3877d833d1a
stevleibelt <stev@leibelt.de>
2013-12-06 17:58:59 +0100

    what ever

---
+++ source/Net/Bazzline/Component/Filesystem/FilesystemObjectInterface.php
@@ -0,0 +1,300 @@
+<?php
+/**
+ * @author stev leibelt <artodeto@arocr.de>
+ * @since 2013-04-25
+ */
+
+namespace Net\Bazzline\Component\Filesystem;
+
+/**
+ * Class FilesystemInterface
+ * Generic interface for filesystem item
+ *
+ * @package Net\Bazzline\Component\Filesystem
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-05-17
+ */
+interface FilesystemObjectInterface extends DACInterface
+{
+    /**
+     * @param string $path
+     * @return FilesystemObjectInterface
+     * @throws InputOutputException
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-06-16
+     */
+    public function __construct($path);
+
+    /**
+     * Sets the name of the filesystem item
+     *
+     * @param string $name - name of the item
+     *
+     * @return FilesystemObjectInterface
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-05-17
+     */
+    public function setName($name);
+
+    /**
+     * Returns the name of the filesystem item or null if no name is set.
+     *
+     * @return null|string
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-05-17
+     */
+    public function getName();
+
+    /**
+     * Sets the path of the filesystem item.
+     *
+     * @param string $path - path of the item (relative or absolute)
+     *
+     * @return FilesystemObjectInterface
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-05-17
+     */
+    public function setPath($path);
+
+    /**
+     * Returns the path of the filesystem item or null if no path is set.
+     *
+     * @return null|string
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-05-17
+     */
+    public function getPath();
+
+    /**
+     * Validates if the write flag is set for this filesystem item.
+     *
+     * @param string $userGroup - ugoa are supported
+     * @return boolean
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-05-17
+     */
+    public function isWritable($userGroup = 'u');
+
+    /**
+     * Tries to set the write flag
+     *
+     * @param string $userGroup - ugoa are supported
+     * @return boolean
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-06-18
+     */
+    public function setIsWritable($userGroup = 'u');
+
+    /**
+     * Tries to set the write flag
+     *
+     * @param string $userGroup - ugoa are supported
+     * @return boolean
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-06-18
+     */
+    public function unsetIsWritable($userGroup = 'u');
+
+    /**
+     * Validates if the read flag is set for this filesystem item.
+     *
+     * @param string $userGroup - ugoa are supported
+     * @return boolean
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-05-17
+     */
+    public function isReadable($userGroup = 'u');
+
+    /**
+     * Tries to set the read flag
+     *
+     * @param string $userGroup - ugoa are supported
+     * @return boolean
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-06-18
+     */
+    public function setIsReadable($userGroup = 'u');
+
+    /**
+     * Tries to set the read flag
+     *
+     * @param string $userGroup - ugoa are supported
+     * @return boolean
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-06-18
+     */
+    public function unsetIsReadable($userGroup = 'u');
+
+    /**
+     * Validates if the execute flag is set for this filesystem item.
+     *
+     * @param string $userGroup - ugoa are supported
+     * @return boolean
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-05-17
+     */
+    public function isExecutable($userGroup = 'u');
+
+    /**
+     * Tries to set the execute flag
+     *
+     * @param string $userGroup - ugoa are supported
+     * @return boolean
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-06-18
+     */
+    public function setIsExecutable($userGroup = 'u');
+
+    /**
+     * Tries to set the execute flag
+     *
+     * @param string $userGroup - ugoa are supported
+     * @return boolean
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-06-18
+     */
+    public function unsetIsExecutable($userGroup = 'u');
+
+    /**
+     * Validates if filesystem item has content.
+     *
+     * @return boolean
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-05-17
+     */
+    public function hasContent();
+
+    /**
+     * Returns the content of the filesystem item.
+     *
+     * @return string|ItemCollection
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-05-17
+     */
+    public function getContent();
+
+    /**
+     * Sets toe content fo the filesystem item.
+     *
+     * @param string|ItemCollection $content - A string or the
+     *  FilesystemCollection
+     *
+     * @return FilesystemObjectInterface
+     * @throws InputOutputException
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-05-17
+     */
+    public function setContent($content);
+
+    /**
+     * @param string $content
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-05-09
+     */
+    public function appendContent($content);
+
+    /**
+     * @param string $content
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-05-09
+     */
+    public function prependContent($content);
+
+    /**
+     * Validates if filesystem item exists on filesystem or not.
+     *
+     * @return boolean
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-05-17
+     */
+    public function isNew();
+
+    /**
+     * Validates if filesystem item is modified and not saved.
+     *
+     * @return boolean
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-05-17
+     */
+    public function isModified();
+
+    /**
+     * Returns last modification time or null if item is new.
+     *
+     * @return null|int
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-05-17
+     */
+    public function getModificationTime();
+
+    /**
+     * Returns last access time or null if item is new.
+     *
+     * @return null|int
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-05-17
+     */
+    public function getAccessTime();
+
+    /**
+     * Returns create time or null if item is new.
+     *
+     * @return null|int
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-05-17
+     */
+    public function getCreateTime();
+
+    /**
+     * Returns last modification date or null if item is null.
+     *
+     * @param string $format - date format by php date()
+     *
+     * @return null|int
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-05-17
+     */
+    public function getModificationDate($format = 'Y-m-d H:i:s');
+
+    /**
+     * Returns last access date or null if item is null.
+     *
+     * @param string $format - date format by php date()
+     *
+     * @return null|int
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-05-17
+     */
+    public function getAccessDate($format = 'Y-m-d H:i:s');
+
+    /**
+     * Returns create date or null if item is null.
+     *
+     * @param string $format - date format by php date()
+     *
+     * @return null|int
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-05-17
+     */
+    public function getCreateDate($format = 'Y-m-d H:i:s');
+
+    /**
+     * Saved content and throws exception if item is not new.
+     * Throws exception if item is not writable.
+     *
+     * @param bool $override
+     * @return int
+     * @throws InputOutputException
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-05-17
+     */
+    public function save($override = false);
+
+    /**
+     * @return bool
+     * @throws InputOutputException
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-05-17
+     */
+    public function delete();
+}
