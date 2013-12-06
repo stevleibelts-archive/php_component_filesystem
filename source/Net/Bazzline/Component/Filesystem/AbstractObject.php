684bf495cb6a5cceff843b5e02432f43504cf0e5
stevleibelt <stev@leibelt.de>
2013-12-06 18:14:54 +0100

    renamed

---
+++ source/Net/Bazzline/Component/Filesystem/AbstractObject.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-12-06
+ */
+
+namespace Net\Bazzline\Component\Filesystem;
+
+/**
+ * Class AbstractObject
+ *
+ * @package Net\Bazzline\Component\Filesystem
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-12-06
+ */
+abstract class AbstractObject
+{
+    /**
+     * @var string
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-12-06
+     */
+    protected $path;
+
+    /**
+     * @param string $path
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-12-06
+     */
+    public function setPath($path)
+    {
+        $this->path = (string) $path;
+    }
+
+    /**
+     * @return string
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-12-06
+     */
+    public function getPath()
+    {
+        return $this->path;
+    }
+
+    /**
+     * @return bool
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-12-06
+     */
+    abstract function isNew();
+
+    /**
+     * @return bool
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-12-06
+     */
+    abstract function isModified();
+}
