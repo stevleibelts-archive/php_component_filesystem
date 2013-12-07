b6c30ebeeac279ad5c30c9faadd005e7cc76e96c
stevleibelt <stev@leibelt.de>
2013-12-07 18:35:23 +0100

    added idea as todo

--- source/Net/Bazzline/Component/Filesystem/Filesystem.php
+++ source/Net/Bazzline/Component/Filesystem/Filesystem.php
@@ -6,6 +6,14 @@
 
 namespace Net\Bazzline\Component\Filesystem;
 
+/**
+ * Class Filesystem
+ *
+ * @package Net\Bazzline\Component\Filesystem
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-12-06
+ * @todo chmod and so on as decorators?
+ */
 class Filesystem
 {
     public function getPathObjectCollection($path, $options)
