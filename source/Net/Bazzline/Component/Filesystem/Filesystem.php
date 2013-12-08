4169dc3782eab0277d5d10e705566451f8d406b1
stevleibelt <stev@leibelt.de>
2013-12-08 18:07:26 +0100

    startet implemementing copy

--- source/Net/Bazzline/Component/Filesystem/Filesystem.php
+++ source/Net/Bazzline/Component/Filesystem/Filesystem.php
@@ -16,10 +16,37 @@ namespace Net\Bazzline\Component\Filesystem;
  */
 class Filesystem
 {
+    public function copy($original, $target, $override = false)
+    {
+        $originalObject = ($original instanceof AbstractObject)
+            ? $original : $this->createObjectFromPath($original);
+        $targetObject = ($target instanceof AbstractObject)
+            ? $target : $this->createObjectFromPath($target);
+
+    }
+
     public function getPathObjectCollection($path, $options)
     {
 
     }
 
-
-} 
+    /**
+     * @param string $path
+     * @return AbstractObject|Directory|File
+     * @throws InvalidArgumentException
+     * @author stev leibelt <artodeto@arcor.de>
+     * @since 2013-12-08
+     */
+    private function createObjectFromPath($path)
+    {
+        if (is_file($path)) {
+            return new File($path);
+        } else if (is_dir($path)) {
+            return new Directory($path);
+        } else {
+            throw new InvalidArgumentException(
+                'provided path "' . $path . '" is neither a directory nor file'
+            );
+        }
+    }
+}
