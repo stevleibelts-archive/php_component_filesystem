6cb03a4d2ae25070072080bf1c0eb6364cf4daa8
stevleibelt <stev@leibelt.de>
2013-12-08 18:04:55 +0100

    Added invalid arugment exception

---
+++ source/Net/Bazzline/Component/Filesystem/InvalidArgumentException.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-12-08
+ */
+
+namespace Net\Bazzline\Component\Filesystem;
+
+use InvalidArgumentException as ParentClass;
+
+/**
+ * Class InvalidArgumentException
+ *
+ * @package Net\Bazzline\Component\Filesystem
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-12-08
+ */
+class InvalidArgumentException extends ParentClass
+{
+} 
