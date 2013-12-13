bfdb17df4766fae5f224e4783abe9327389b33e8
stevleibelt <stev@leibelt.de>
2013-12-14 00:09:57 +0100

    refactored filesystem object collection

---
+++ source/Net/Bazzline/Component/Filesystem/FilesystemObjectCollection.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-12-06
+ */
+
+namespace Net\Bazzline\Component\Filesystem;
+
+use SplObjectStorage;
+
+/**
+ * Class ObjectCollection
+ *
+ * @package Net\Bazzline\Component\Filesystem
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-12-06
+ */
+class FilesystemObjectCollection extends SplObjectStorage
+{
+}
