1d5f79abf63c3804031d3be2a382a811b23dba15
stevleibelt <stev@leibelt.de>
2013-12-14 02:08:01 +0100

    finishd basic file example

--- examples/example/File.php
+++ examples/example/File.php
@@ -15,10 +15,14 @@ $class = get_class($file);
 $methods = get_class_methods($class);
 
 foreach ($methods as $method) {
+    if ($method === 'getLinkTarget') {
+        continue;
+    }
     $startsWithGet = (strpos($method, 'get') === 0);
     $startsWithIs = (strpos($method, 'is') === 0);
+
     if ($startsWithGet
         || $startsWithIs) {
-        echo $method . ': ' . $file->$method() . PHP_EOL;
+        echo $method . ': ' . var_export($file->$method(), true) . PHP_EOL;
     }
 }
