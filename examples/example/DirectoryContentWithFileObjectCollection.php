16b7383c67466314b3655fc63828a5d5f06c5f3c
stevleibelt <stev@leibelt.de>
2013-12-14 19:05:30 +0100

    added DirectoryContentWithFileObjectCollection

---
+++ examples/example/DirectoryContentWithFileObjectCollection.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-12-14
+ */
+
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+use Net\Bazzline\Component\Filesystem\Filesystem;
+use Net\Bazzline\Component\Filesystem\FileObjectCollection;
+
+$fileSystem = new FileSystem();
+$directory = $fileSystem->createDirectoryObject(__DIR__);
+
+$class = get_class($directory);
+$methods = get_class_methods($class);
+
+$content = $directory->getContent('', new FileObjectCollection());
+echo 'content class: ' . get_class($content) . PHP_EOL;
+echo 'number of entries: ' . $content->count() . PHP_EOL;
+echo 'iterating over content' . PHP_EOL;
+
+foreach ($content as $item) {
+    /**
+     * @var \Net\Bazzline\Component\Filesystem\AbstractFilesystemObject $item
+     */
+    echo 'class: ' . get_class($item) . PHP_EOL;
+    echo 'path: ' . $item->getRealPath() . PHP_EOL;
+}
