932460918ea326f878d89c9271a63bd91ca9133c
stevleibelt <stev@leibelt.de>
2013-12-16 00:02:17 +0100

    finished scribbled idea

--- source/Net/Bazzline/Component/Filesystem/FilesystemObjectCollectionWithFilter.php
+++ source/Net/Bazzline/Component/Filesystem/FilesystemObjectCollectionWithFilter.php
@@ -14,9 +14,17 @@ class FilesystemObjectCollectionWithFilter extends FilesystemObjectCollection
      * @param $filter
      * @author stev leibelt <artodeto@arcor.de>
      * @since 2013-12-16
+     * @todo or move into ctor?
      */
     public function setFilter($filter)
     {
         $this->filter = $filter;
+    }
+
+    public function attach($object, $data = null)
+    {
+        if ($this->filter($object)) {
+            parent::attach($object, $data);
+        }
     }
 }
