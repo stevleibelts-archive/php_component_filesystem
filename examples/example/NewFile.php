4ad7e79e4d7e14d90e309300913d504dcb822599
stevleibelt <stev@leibelt.de>
2013-12-17 00:03:56 +0100

    created new example

---
+++ examples/example/NewFile.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @author stev leibelt <artodeto@arcor.de>
+ * @since 2013-12-14
+ */
+
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+use Net\Bazzline\Component\Filesystem\Filesystem;
+
+$fileSystem = new FileSystem();
+$file = $fileSystem->createFileObject(__DIR__ . DIRECTORY_SEPARATOR . 'testFile_' . time());
+
+$class = get_class($file);
+$methods = get_class_methods($class);
+
+foreach ($methods as $method) {
+    if ($method === 'getLinkTarget') {
+        continue;
+    }
+    $startsWithGet = (strpos($method, 'get') === 0);
+    $startsWithIs = (strpos($method, 'is') === 0);
+
+    if ($startsWithGet
+        || $startsWithIs) {
+        echo $method . ': ' . var_export($file->$method(), true) . PHP_EOL;
+    }
+}
